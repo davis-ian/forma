32e6121b2edcd7abd4f4ccbfe46d7982d01526ae
Ian Davis <iandavis6481@gmail.com>
2025-04-16 18:37:45 -0500

    leven generation notes

---
+++ src/gameplay/level/Room.ts
@@ -0,0 +1,16 @@
+/*
+
+
+Takes position (x, y), direction from parent
+
+Randomizes:
+
+width, height
+
+exits: maybe 2–4 exits randomly
+
+flags like hasEnemies, isBossRoom, etc.
+
+Returns a Room
+
+*/
