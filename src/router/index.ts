0e44682f8bcef423ea279d8c6a6080c6576fc75d
Ian Davis <iandavis6481@gmail.com>
2025-04-27 08:32:25 -0500

    Removed logo from HomeScreen

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import HomeScreen from '@/ui/HomeScreen.vue'
 import GameScreen from '@/ui/GameScreen.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: HomeScreen },
-  { path: '/game', name: 'game', component: GameScreen },
+    { path: '/', name: 'home', component: HomeScreen },
+    { path: '/game', name: 'game', component: GameScreen },
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 })
